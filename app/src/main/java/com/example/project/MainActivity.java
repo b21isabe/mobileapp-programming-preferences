34166299a02c203caa787820fc2733d381e1bbd7
Isak Bergström <b21isabe@student.his.se>
2023-05-05 01:57:31 +0200

    Skapat en ClickListener i MainActivity som skickar en till SecondActivity när knappen trycks

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private SharedPreferences PrefRef;
-    private SharedPreferences.Editor PrefEditor;
+    //private SharedPreferences PrefRef;
+    //private SharedPreferences.Editor PrefEditor;
+
+
 
 
 
@@ -20,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button MainButton = findViewById(R.id.MainButton);
+        MainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
 
